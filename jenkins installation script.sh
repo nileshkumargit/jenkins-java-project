8c566db9f0d556881f344f33d8d2aba35f7cbb20
nileshkumargit <nileshkumar002111@gmail.com>
2026-02-10 19:48:46 +0530

    Update jenkins installation script.sh

--- jenkins installation script.sh
+++ jenkins installation script.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+# Add Jenkins repository
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-# Add required dependencies for the jenkins package
 
-yum install java-17-amazon-corretto jenkins -y
-sudo yum install jenkins -y
-sudo systemctl restart jenkins
+# Import Jenkins GPG key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+#upgrade the system to install packages.
+sudo yum upgrade
+
+# Install Java 17 (required) and Jenkins
+sudo yum install java-17-amazon-corretto jenkins -y
+
+# Reload systemd, enable and start Jenkins
 sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
